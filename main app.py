d50bdf5221db344309436d87f563ede4db3063d2
Jbonesy23 <123136384+Jbonesy23@users.noreply.github.com>
2023-12-11 11:35:52 -0500

    Update main app.py

---
+++ main app.py
@@ -0,0 +1,556 @@
+[
+    {
+        "type": 0,
+        "key": "scripts",
+        "value": "deploy"
+    },
+    {
+        "type": 0,
+        "key": [
+            "package.json --version dev --region us-east-1 --api-module api"
+        ],
+        "value": "package.json"
+    },
+    {
+        "type": 0,
+        "key": [
+            "scripts"
+        ],
+        "value": "deploy"
+    },
+    {
+        "type": 0,
+        "key": [
+            "package.json create --version dev --region us-east-1 --api-module api"
+        ],
+        "value": "release"
+    },
+    {
+        "type": 0,
+        "key": [
+            "package.json set-version --version production"
+        ],
+        "value": "start"
+    },
+    {
+        "type": 0,
+        "key": [
+            "ntl"
+        ],
+        "value": "#comment"
+    },
+    {
+        "type": 0,
+        "key": [
+            ""
+        ],
+        "value": "html"
+    },
+    {
+        "type": 0,
+        "key": [
+            "@lang"
+        ],
+        "value": "en"
+    },
+    {
+        "type": 0,
+        "key": [
+            "head"
+        ],
+        "value": "meta"
+    },
+    {
+        "type": 0,
+        "key": [
+            "@charset"
+        ],
+        "value": "UTF-8"
+    },
+    {
+        "type": 0,
+        "key": [
+            "@http-equiv"
+        ],
+        "value": "X-UA-Compatible"
+    },
+    {
+        "type": 0,
+        "key": [
+            "@content"
+        ],
+        "value": "IE=edge"
+    },
+    {
+        "type": 0,
+        "key": [
+            "@name"
+        ],
+        "value": "viewport"
+    },
+    {
+        "type": 0,
+        "key": [
+            "@content"
+        ],
+        "value": "width=device-width, initial-scale=1.0"
+    },
+    {
+        "type": 0,
+        "key": [
+            "title"
+        ],
+        "value": "Panda Core - The Next Babydoge Coin"
+    },
+    {
+        "type": 0,
+        "key": [
+            "#comment"
+        ],
+        "value": ""
+    },
+    {
+        "type": 0,
+        "key": [
+            "link"
+        ],
+        "value": "@rel"
+    },
+    {
+        "type": 0,
+        "key": [
+            "shortcut icon"
+        ],
+        "value": "@href"
+    },
+    {
+        "type": 0,
+        "key": [
+            "./favicon.svg"
+        ],
+        "value": "@type"
+    },
+    {
+        "type": 0,
+        "key": [
+            "image/svg+xml"
+        ],
+        "value": "@rel"
+    },
+    {
+        "type": 0,
+        "key": [
+            "stylesheet"
+        ],
+        "value": "@href"
+    },
+    {
+        "type": 0,
+        "key": [
+            "./assets/css/style.css"
+        ],
+        "value": "@rel"
+    },
+    {
+        "type": 0,
+        "key": [
+            "preconnect"
+        ],
+        "value": "@href"
+    },
+    {
+        "type": 0,
+        "key": [
+            "https://fonts.googleapis.com"
+        ],
+        "value": "@rel"
+    },
+    {
+        "type": 0,
+        "key": [
+            "preconnect"
+        ],
+        "value": "@href"
+    },
+    {
+        "type": 0,
+        "key": [
+            "https://fonts.gstatic.com"
+        ],
+        "value": "@crossorigin"
+    },
+    {
+        "type": 0,
+        "key": [
+            "anonymous"
+        ],
+        "value": "@href"
+    },
+    {
+        "type": 0,
+        "key": [
+            "https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&amp;display=swap"
+        ],
+        "value": "@rel"
+    },
+    {
+        "type": 0,
+        "key": [
+            "stylesheet"
+        ],
+        "value": "@rel"
+    },
+    {
+        "type": 0,
+        "key": [
+            "stylesheet"
+        ],
+        "value": "@href"
+    },
+    {
+        "type": 0,
+        "key": [
+            "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        ],
+        "value": "@rel"
+    },
+    {
+        "type": 0,
+        "key": [
+            "stylesheet"
+        ],
+        "value": "@href"
+    },
+    {
+        "type": 0,
+        "key": [
+            "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+        ],
+        "value": "@rel"
+    },
+    {
+        "type": 0,
+        "key": [
+            "stylesheet"
+        ],
+        "value": "@href"
+    },
+    {
+        "type": 0,
+        "key": [
+            "https://cdn.materialdesignicons.com/5.4.55/css/materialdesignicons.min.css"
+        ],
+        "value": "body"
+    },
+    {
+        "type": 0,
+        "key": [
+            "#comment"
+        ],
+        "value": ""
+    },
+    {
+        "type": 0,
+        "key": [
+            "header"
+        ],
+        "value": "@class"
+    },
+    {
+        "type": 0,
+        "key": [
+            "header"
+        ],
+        "value": "@data-*"
+    },
+    {
+        "type": 0,
+        "key": [
+            "value"
+        ],
+        "value": "div"
+    },
+    {
+        "type": 0,
+        "key": [
+            "@class"
+        ],
+        "value": "container"
+    },
+    {
+        "type": 0,
+        "key": [
+            "a"
+        ],
+        "value": {
+            "type": 10,
+            "value": [
+                {
+                    "type": 0,
+                    "key": "@href",
+                    "value": "#"
+                },
+                {
+                    "type": 0,
+                    "key": [
+                        "@class"
+                    ],
+                    "value": "logo"
+                },
+                {
+                    "type": 0,
+                    "key": [
+                        "img"
+                    ],
+                    "value": {
+                        "type": 10,
+                        "value": [
+                            {
+                                "type": 0,
+                                "key": "@src",
+                                "value": "https://telegra.ph/file/0c770600ad44e2b3279d7.jpg"
+                            },
+                            {
+                                "type": 0,
+                                "key": [
+                                    "@width"
+                                ],
+                                "value": "32"
+                            },
+                            {
+                                "type": 0,
+                                "key": [
+                                    "@height"
+                                ],
+                                "value": "32"
+                            },
+                            {
+                                "type": 0,
+                                "key": [
+                                    "@alt"
+                                ],
+                                "value": "Panda Core logo"
+                            }
+                        ]
+                    }
+                },
+                {
+                    "type": 0,
+                    "key": [
+                        "#text"
+                    ],
+                    "value": "Panda Core"
+                }
+            ]
+        }
+    },
+    {
+        "type": 0,
+        "key": [
+            "nav"
+        ],
+        "value": {
+            "type": 10,
+            "value": [
+                {
+                    "type": 0,
+                    "key": "@class",
+                    "value": "navbar"
+                },
+                {
+                    "type": 0,
+                    "key": [
+                        "@data-*"
+                    ],
+                    "value": "navbar"
+                },
+                {
+                    "type": 0,
+                    "key": [
+                        "ul"
+                    ],
+                    "value": {
+                        "type": 10,
+                        "value": [
+                            {
+                                "type": 0,
+                                "key": "@class",
+                                "value": "navbar-list"
+                            },
+                            {
+                                "type": 0,
+                                "key": [
+                                    "li"
+                                ],
+                                "value": [
+                                    {
+                                        "type": 10,
+                                        "value": [
+                                            {
+                                                "type": 0,
+                                                "key": "@class",
+                                                "value": "navbar-item"
+                                            },
+                                            {
+                                                "type": 0,
+                                                "key": [
+                                                    "a"
+                                                ],
+                                                "value": {
+                                                    "type": 10,
+                                                    "value": [
+                                                        {
+                                                            "type": 0,
+                                                            "key": "@href",
+                                                            "value": "#"
+                                                        },
+                                                        {
+                                                            "type": 0,
+                                                            "key": [
+                                                                "@class"
+                                                            ],
+                                                            "value": "navbar-link active"
+                                                        },
+                                                        {
+                                                            "type": 0,
+                                                            "key": [
+                                                                "@data-*"
+                                                            ],
+                                                            "value": "nav-link"
+                                                        },
+                                                        {
+                                                            "type": 0,
+                                                            "key": [
+                                                                "#text"
+                                                            ],
+                                                            "value": "HOME"
+                                                        }
+                                                    ]
+                                                }
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        "type": 10,
+                                        "value": [
+                                            {
+                                                "type": 0,
+                                                "key": "@class",
+                                                "value": "navbar-item"
+                                            },
+                                            {
+                                                "type": 0,
+                                                "key": [
+                                                    "a"
+                                                ],
+                                                "value": "href=\"https://panda-core.gitbook.io/panda-core-white-paper-v1.-0\" class=data-*\"nav-link\" =target=_blank\" White Paper"
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        "type": 10,
+                                        "value": [
+                                            {
+                                                "type": 0,
+                                                "key": "@class",
+                                                "value": "navbar-item"
+                                            },
+                                            {
+                                                "type": 0,
+                                                "key": [
+                                                    "a"
+                                                ],
+                                                "value": "href=\"#\" class=\"navbar-link\" data-*=\"nav-link Presale"
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        "type": 10,
+                                        "value": [
+                                            {
+                                                "type": 0,
+                                                "key": "@class",
+                                                "value": "navbar-item"
+                                            },
+                                            {
+                                                "type": 0,
+                                                "key": [
+                                                    "a"
+                                                ],
+                                                "value": "href=\"https://icecreamswap.com/swap?chainId=1116\"&amp\";outputCurrency=0x6392f055A691C1a3913E6Ee4D41AEc00CbCFe49b\" class=\"navbar-link\" data-*=\"nav-link\" \"target=\"_blank\"Buy/Sell"
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        "type": 10,
+                                        "value": [
+                                            {
+                                                "type": 0,
+                                                "key": "@class",
+                                                "value": "navbar-item"
+                                            },
+                                            {
+                                                "type": 0,
+                                                "key": [
+                                                    "a"
+                                                ],
+                                                "value": "href=\"#roadmap\" class=\"navbar-link\" data-*=\"nav-link\"Roadmap"
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        "type": 10,
+                                        "value": [
+                                            {
+                                                "type": 0,
+                                                "key": "@class",
+                                                "value": "navbar-item"
+                                            },
+                                            {
+                                                "type": 0,
+                                                "key": [
+                                                    "a"
+                                                ],
+                                                "value": "href=\"#faqs\" class=\"navbar-link\" data-*=nav-link\"FAQs"
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    },
+    {
+        "type": 0,
+        "key": [
+            "button"
+        ],
+        "value": {
+            "type": 10,
+            "value": [
+                {
+                    "type": 0,
+                    "key": "@class",
+                    "value": "nav-toggle-btn"
+                },
+                {
+                    "type": 0,
+                    "key": [
+                        "@aria-label"
+                    ],
+                    "value": "Toggle menu"
+                },
+                {
+                    "type": 0,
+                    "key": [
+                        "@data-*"
+                    ],
+                    "value": "nav-toggler"
+                }
+            ]
+        }
+    }
+]
